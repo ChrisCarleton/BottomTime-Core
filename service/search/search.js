e9e8f3ba336ddc7f87f4152b5dd4013de4a15510
Chris Carleton <mrchriscarleton@gmail.com>
2019-05-20 16:27:40 -0400

    Properly initializing ES client.

--- service/search/search.js
+++ service/search/search.js
@@ -1,8 +1,27 @@
 import { Client } from 'elasticsearch';
 import config from '../config';
+import { esLogger as log } from '../logger';
+
+class ESLogger {
+	constructor() {
+		this.logger = log;
+		this.error = log.error.bind(log);
+		this.warning = log.warn.bind(log);
+		this.info = log.info.bind(log);
+		this.debug = log.debug.bind(log);
+		this.trace = log.trace.bind(log);
+		this.close = () => {
+			/* Bunyan loggers do not need to be closed. */
+		};
+	}
+}
 
 const client = new Client({
-	node: config.elasticSearchEndpoint
+	host: config.elasticSearchEndpoint,
+	sniffOnStart: true,
+	sniffInterval: 300000,
+	connectionClass: 'http',
+	log: ESLogger
 });
 
 export default client;
