c7bca55e42c68a5a08359fc2831b8b7794ae64ef
Chris Carleton <chris.carleton@vidyard.com>
2020-02-28 14:26:15 -0500

    admin/init-local.sh now invokes yarn instead of npm.

--- admin/init-local.sh
+++ admin/init-local.sh
@@ -9,4 +9,4 @@ docker run --name bt_es -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-n
 docker run --name bt_s3 -d -p 4569:4569 lphoward/fake-s3:latest
 docker run --name bt_smtp -d -p 15025:25 tecnativa/smtp-sink:latest
 
-npm run init-local
+yarn init-local
