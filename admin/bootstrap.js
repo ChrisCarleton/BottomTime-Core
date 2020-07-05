1466b8e4357fa64a952d8f8f8e9574d034a97f62
Chris Carleton <chris.carleton@vidyard.com>
2020-07-05 18:44:59 -0400

    Admin script bootstrapper will now load .env variables.

--- admin/bootstrap.js
+++ admin/bootstrap.js
@@ -3,5 +3,6 @@
  * be run directly. See the section on Admin Scripts in the README.md file.
  **********************************************************************************/
 
+require('dotenv').config();
 require('@babel/register');
 require(process.argv[2]);
